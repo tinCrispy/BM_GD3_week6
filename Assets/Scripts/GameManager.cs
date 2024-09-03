dfd99023d2d66c38a1bca58e6bd561741ce8c360
tinCrispy <basilmarples@outlook.com>
2024-09-03 16:06:47 +0100

    tricks and treats
    background became the floor
    click items to get points
    pump the volume up

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.PlayerLoop.EarlyUpdate;
+
 
 public class GameManager : MonoBehaviour
 {
+    public TMP_Text scoreText;
+    private int score;
+
     public GameObject[] targetprefabs;
     private int spawnRate = 1;
+
+    public Slider musicVolume;
+    public AudioSource soundTrack;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnRandomiser());
+        score = 0;
+        ScoreUpdate(0);
+        scoreText.text = ("nah bruv");
+
+        soundTrack = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeVolume(); 
     }
 
     IEnumerator SpawnRandomiser()
@@ -25,9 +44,23 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int targetIndex = Random.Range(0, targetprefabs.Length);
             Instantiate(targetprefabs[targetIndex]);
+
+            
         }
 
     }
 
+
+    public void ScoreUpdate(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = ("Score: " + score);
+
+    }
+
+    public void ChangeVolume()
+    {
+        soundTrack.volume = musicVolume.value;
+    }
    
 }
--- Assets/Scripts/Targets.cs
+++ Assets/Scripts/Targets.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Targets : MonoBehaviour
 {
     public Rigidbody rb;
-    private float minspeed = 12;
-    private float maxspeed = 16;
+    private float minspeed = 5;
+    private float maxspeed = 10;
     private float xRange = 4;
     private float maxTorque = 10;
-    private float ySpawnPos = -0.5f;
+    private float ySpawnPos = 7;
+
+    private GameManager gameManager;
+    public AudioClip soundTrack;
+
+   
+
+
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +28,8 @@ public class Targets : MonoBehaviour
         rb.AddForce( RandomForce(), ForceMode.Impulse);
         rb.AddTorque (RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -44,6 +56,9 @@ public class Targets : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.ScoreUpdate(1);
+        
+
 
     }
 }
